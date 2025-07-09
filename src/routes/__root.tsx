4b1e8886c7ffd2cca5b0fdb5dc166c74c406129b
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-07-09 19:28:16 +0530

    add __root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,21 @@
 import { AuthContext, AuthProvider } from "@/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "sonner";
 
 export const Route = createRootRoute({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const queryClient = new QueryClient();
   return (
-    <AuthProvider>
-      <Outlet />
-    </AuthProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <Toaster />
+      </QueryClientProvider>
+    </>
   );
 }
